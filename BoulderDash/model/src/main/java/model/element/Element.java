33b483368ff3be3190fa3fa646281c5358c56527
MazurierPierre <pierre.mazurier@viacesi.fr>
2017-06-22 17:59:43 +0200

    Doc ... Again

--- BoulderDash/model/src/main/java/model/element/Element.java
+++ BoulderDash/model/src/main/java/model/element/Element.java
@@ -7,34 +7,69 @@ import model.Sprite;
 
 public class Element implements IElement {
 
+	// Coordinates relative to the element
 	private int x;
 	private int y;
 
+	// Sprite (Contains images) of the element
 	private Sprite sprite;
+	
+	// Blocks or not the player
 	private model.Permeability permeability;
+	
 	private int index = 0;
 
+	/**
+     * Instantiates a new element.
+     *
+     * @param sprite
+     *            the sprite
+     * @param permeability
+     *            the permeability
+     */
 	public Element(Sprite sprite, model.Permeability permeability) {
 		this.setSprite(sprite);
 		this.setPermeability(permeability);
 	}
 
+	/**
+	 * Returns the sprite related to the element
+	 * 
+	 */
 	public Sprite getSprite() {
 		return sprite;
 	}
 
+	/**
+	 * Sets the sprite of an element
+	 * 
+	 * @param sprite
+	 */
 	public void setSprite(Sprite sprite) {
 		this.sprite = sprite;
 	}
 
+	/**
+	 * Returns the permeability of an element
+	 * 
+	 */
 	public model.Permeability getPermeability() {
 		return permeability;
 	}
 
+	/**
+	 * Sets the permeability of an element
+	 * 
+	 * @param permeability
+	 */
 	public void setPermeability(model.Permeability permeability) {
 		this.permeability = permeability;
 	}
 
+	/**
+	 * Returns the 4 images contained in the sprite
+	 * 
+	 */
 	@Override
 	public Image getImage() {
 
@@ -44,20 +79,36 @@ public class Element implements IElement {
 		return this.sprite.getImage(index++);
 	}
 
-	
-
+	/**
+	 * Returns the x coordinate of the element
+	 * 
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * Sets the x coordinate of the element
+	 * 
+	 * @param x
+	 */
 	public void setX(final int x) {
 		this.x = x;
 	}
 
+	/**
+	 * Returns the y coordinate of the element
+	 * 
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Sets the y coordinate of the element
+	 * 
+	 * @param y
+	 */
 	public void setY(final int y) {
 		this.y = y;
 	}
--- BoulderDash/model/src/main/java/model/element/mobile/Mobile.java
+++ BoulderDash/model/src/main/java/model/element/mobile/Mobile.java
@@ -10,57 +10,136 @@ import model.element.Element;
 
 public abstract class Mobile extends Element implements IMobile {
 
+	/** Alive */
 	private boolean alive = true;
-	// private IMap map;
+	private IMap map;
 	private IBoard board;
 
-	// NEW
+	/**
+	 * Instantiates a new Mobile element
+	 * 
+	 * @param sprite
+	 * @param permeability
+	 */
 	public Mobile(Sprite sprite, model.Permeability permeability) {
 		super(sprite, permeability);
 	}
 
+	/**
+	 * Instantiates a new mobile element
+	 * 
+	 * @param sprite
+	 * @param map
+	 * @param permeability
+	 */
 	public Mobile(Sprite sprite, IMap map, model.Permeability permeability) {
 		super(sprite, permeability);
 	}
 
+	/**
+	 * Instantiates a new Mobile element
+	 * 
+	 * @param x
+	 * @param y
+	 * @param sprite
+	 * @param map
+	 * @param permeability
+	 */
 	public Mobile(int x, int y, Sprite sprite, model.Permeability permeability) {
 		super(sprite, permeability);
 	}
 
+	/**
+	 * Increments the Y coordinate of the element
+	 * 
+	 */
 	public void moveUp() {
 		this.setY(getY() - 1);
 	}
 
+	/**
+	 * Increments the Y coordinate of the element
+	 * 
+	 */
 	public void moveDown() {
 		this.setY(getY() + 1);
 	}
+
+	/**
+	 * Increments the X coordinate of the element
+	 * 
+	 */
 
 	public void moveLeft() {
 		this.setX(this.getX() - 1);
 	}
 
+	/**
+	 * Increments the X coordinate of the element
+	 * 
+	 */
 	public void moveRight() {
 		this.setX(this.getX() + 1);
 	}
 
+	/**
+	 * Does absolutly nothing ... 
+	 * (Used when the player does not hit any key)
+	 * 
+	 */
 	public void doNothing() {
 
 	}
 
-	// Add in UML
+	/**
+	 * Changes the player state (Alvive / Dead)
+	 * 
+	 * @param b
+	 */
+
 	public void setAlive(boolean b) {
 		this.alive = b;
 	}
 
-	// Add in UML
+	/**
+	 * Changes the players state to dead
+	 * 
+	 */
+
 	public void die() {
 		this.alive = false;
 	}
 
+	/**
+	 * Returns the map
+	 * 
+	 * @return
+	 */
+	public IMap getMap() {
+		return map;
+	}
+
+	/**
+	 * Sets the map
+	 * 
+	 * @param map
+	 */
+	public void setMap(IMap map) {
+		this.map = map;
+	}
+
+	/**
+	 * Returns the player state (Alive / Dead)
+	 * 
+	 */
 	public Boolean isAlive() {
 		return alive;
 	}
 
+	/**
+	 * Returns the game board
+	 * 
+	 */
 	public IBoard getBoard() {
 		return board;
 	}
